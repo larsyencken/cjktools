7506571465223a9e52d2d1fdcf9e4d961f911434
Lars Yencken <lljy@csse.unimelb.edu.au>
2007-06-27 13:22:28 +1000

    Added a setup.py file, and moved jptools to src within this folder.
    --HG--
    rename : jptools/__init__.py => src/__init__.py
    rename : jptools/common.py => src/common.py
    rename : jptools/decorators.py => src/decorators.py
    rename : jptools/dyntest.py => src/dyntest.py
    rename : jptools/enum.py => src/enum.py
    rename : jptools/exceptions.py => src/exceptions.py
    rename : jptools/functional/__init__.py => src/functional/__init__.py
    rename : jptools/functional/boolFunc.py => src/functional/boolFunc.py
    rename : jptools/functional/maps.py => src/functional/maps.py
    rename : jptools/functional/sequences.py => src/functional/sequences.py
    rename : jptools/functional/testBoolFunc.py => src/functional/testBoolFunc.py
    rename : jptools/functional/testMaps.py => src/functional/testMaps.py
    rename : jptools/functional/testSequences.py => src/functional/testSequences.py
    rename : jptools/japanese/__init__.py => src/japanese/__init__.py
    rename : jptools/japanese/alternations.py => src/japanese/alternations.py
    rename : jptools/japanese/kanaTable.py => src/japanese/kanaTable.py
    rename : jptools/japanese/scripts.py => src/japanese/scripts.py
    rename : jptools/japanese/testKanaTable.py => src/japanese/testKanaTable.py
    rename : jptools/japanese/testScripts.py => src/japanese/testScripts.py
    rename : jptools/niceThread.py => src/niceThread.py
    rename : jptools/pathlib.py => src/pathlib.py
    rename : jptools/progressBar.py => src/progressBar.py
    rename : jptools/relation.py => src/relation.py
    rename : jptools/sampleDoc.xml => src/sampleDoc.xml
    rename : jptools/shell.py => src/shell.py
    rename : jptools/shellColor.py => src/shellColor.py
    rename : jptools/smartCache.py => src/smartCache.py
    rename : jptools/stats.py => src/stats.py
    rename : jptools/table.py => src/table.py
    rename : jptools/testDecorators.py => src/testDecorators.py
    rename : jptools/testRelation.py => src/testRelation.py
    rename : jptools/testSmartCache.py => src/testSmartCache.py
    rename : jptools/testStats.py => src/testStats.py
    rename : jptools/testTable.py => src/testTable.py
    rename : jptools/testXmlTools.py => src/testXmlTools.py
    rename : jptools/xmlTools.py => src/xmlTools.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------#
+# setup.py
+# Lars Yencken <lars.yencken@gmail.com>
+# vim: ts=4 sw=4 sts=4 et tw=78:
+# Wed Jun 27 12:30:37 2007
+#
+#----------------------------------------------------------------------------#
+
+"""
+Package setup file for the jptools package.
+"""
+
+#----------------------------------------------------------------------------#
+
+from distutils.core import setup
+import os
+
+#----------------------------------------------------------------------------#
+
+jptoolsVersion = u'0.8a'
+hgVersion = os.popen(u'hg id').read().replace(u' ', u'_')
+
+setup(
+        name='jptools',
+        version='%s:%s' % (jptoolsVersion, hgVersion),
+        package_dir={'jptools': 'src'},
+        packages=['jptools', 'jptools.functional', 'jptools.japanese'],
+    )
---
+++ src/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------#
+# __init__.py
+# Lars Yencken <lars.yencken@gmail.com>
+# vim: ts=4 sw=4 sts=4 et tw=78:
+# Mon Oct 24 14:23:55 EST 2005
+#
+#----------------------------------------------------------------------------#
+
+""" This package contains various tools for Japanese NLP tasks, although most
+    are applicable to any python project. See documentation of each module for
+    details.
+"""
+
+#----------------------------------------------------------------------------#
+
+__all__ = [
+    'common',
+    'decorators',
+    'enum',
+    'exceptions',
+    'functional',
+    'japanese',
+    'niceThread',
+    'pathlib',
+    'progressBar',
+    'relation',
+    'shellColor',
+    'shell',
+    'smartCache',
+    'stats',
+    'table',
+    'xmlTools',
+]
+
+#----------------------------------------------------------------------------#
+
